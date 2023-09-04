c69acd3a8401df3c4e4993a37b0b29e6a54b2d4f
Dharmapra2 <dharmapra865@gmail.com>
2023-09-04 21:49:18 +0530

    conn

--- index.js
+++ index.js
@@ -7,7 +7,25 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-const esClient = new Client({ node: "https://localhost:9200" });
+const esClient = new Client({
+  node: "https://localhost:9200",
+  auth: {
+    username: "elastic",
+    password: "txBxb3-0QO4R7-tFXvXqMKewt8",
+    // apiKey: "el9YbllJb0JobmZjUFJiSUx3OG46ZEFLaFpNN3NRWDZGcFFZbGtmY01Ddw==",
+  },
+});
+const result = esClient.search(
+  {
+    index: "test-message",
+    query: {
+      match: { hello: "world" },
+    },
+  },
+  { meta: true }
+);
+
+console.log(result);
 
 app.get("/", (req, res) => {
   console.log("hi");
@@ -15,7 +33,7 @@ app.get("/", (req, res) => {
 });
 app.get("/hi/34", (req, res) => {
   console.log("987 8765");
-  res.json({ msg: "hi oiuyg!", data: esClient });
+  res.json({ msg: "hi jhgf!", data: esClient });
 });
 
 app.listen(port, () => {
